b764bcfb78fefe2935848e8e212105e18d333bc8
austinh13 <austinha236@gmail.com>
2025-08-15 17:12:44 -0500

    remove added to education

--- CV-Gen/src/components/EducationCV.jsx
+++ CV-Gen/src/components/EducationCV.jsx
@@ -2,12 +2,20 @@ export default function EducationCV({ sections }) {
   return (
     <div className="educationSection">
       {sections.map((sec, index) => (
-        <div key={index} className="educationItem">
-          <h4>{sec.school}, {sec.city}</h4>
-          <p>A {sec.degree} Degree</p>
-          <p>From: {sec.sDate} To: {sec.eDate}</p>
-          <p>GPA: {sec.gpa}</p>
+        
+        <div className = "educationTab">
+          <div key={index} className="educationItem">
+            <h4>{sec.school}, {sec.city}</h4>
+            <p>A {sec.degree} Degree</p>
+            <p>From: {sec.sDate} To: {sec.eDate}</p>
+            <p>GPA: {sec.gpa}</p>
+          </div>
+          <button className = "deleteEducation" onClick={
+              (e) => e.target.parentNode.remove()}>
+          Delete</button>
+
         </div>
+        
       ))}
     </div>
   );
